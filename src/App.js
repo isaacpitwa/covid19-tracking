0061f75ebd1eb011899f3e0a95d1f8996f252ca5
isaacpitwa <isaacpitwa256@gmail.com>
2022-05-18 00:24:17 +0300

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Metrics from './components/Metrics';
 
 function App() {
   return (
-    <>
-      The Application Renders Properly
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={<Metrics />}
+        exact
+      />
+
+    </Routes>
   );
 }
 
---
+++ src/components/Metrics.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Metrics() {
+  return (
+    <p>Metrics should show</p>
+  );
+}
+export default Metrics;
